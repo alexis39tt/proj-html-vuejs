1c65f6bbcf8b61e04b097cd2d61ca1307522d9c2
alexis39tt <alessandro.fonzi@gmail.com>
2022-04-30 00:28:25 +0200

    added button to scroll back to top

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,65 @@
 <template>
   <div id="app">
-    <HeaderSection/>
-    <MainSection/>
-    <FooterSection/>
+    <HeaderSection />
+    <ScrollToTop />
+    <MainSection />
+    <FooterSection />
   </div>
 </template>
 
 <script>
-import HeaderSection from "./components/HeaderSection.vue"
-import MainSection from "./components/MainSection.vue"
-import FooterSection from "./components/FooterSection.vue"
+import HeaderSection from "./components/HeaderSection.vue";
+import ScrollToTop from "./components/ScrollToTop.vue";
+import MainSection from "./components/MainSection.vue";
+import FooterSection from "./components/FooterSection.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HeaderSection,
+    ScrollToTop,
     MainSection,
-    FooterSection
-  }
-}
+    FooterSection,
+  },
+};
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Roboto:wght@300;400;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Roboto:wght@300;400;700&display=swap");
 @import "mixin";
 
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-html{
+html {
   font-size: 22px;
+  scroll-behavior: smooth;
 }
 
-ul{
+ul {
   list-style: none;
 }
 
-::-webkit-scrollbar{
+::-webkit-scrollbar {
   width: 8px;
 }
 
-::-webkit-scrollbar-track{
+::-webkit-scrollbar-track {
   background: $lightgray;
 }
 
-::-webkit-scrollbar-thumb{
+::-webkit-scrollbar-thumb {
   background: $lightcyan;
 }
 
-::-webkit-scrollbar-thumb:hover{
+::-webkit-scrollbar-thumb:hover {
   background: $darkcyan;
 }
 
-::selection{
+::selection {
   background: $lightcyan;
 }
 </style>
---
+++ src/components/ScrollToTop.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="scroll-to-top" @click="backtotop()">
+    <img src="../assets/svg/top.svg" alt="scroll to top" />
+    <p>TOP</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ScrollToTop",
+  methods: {
+      backtotop: function(){
+          window.scrollTo(0, 0)
+      }
+  },
+  mounted() {
+    let button = document.querySelector("#scroll-to-top");
+    window.onscroll = () => {
+      if (window.scrollY > 180) {
+        button.classList.add("visible");
+      } else {
+        button.classList.remove("visible");
+      }
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../mixin";
+
+#scroll-to-top {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: 90px;
+  height: 90px;
+  background: $lightcyan;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: $white;
+  letter-spacing: 2px;
+  font-weight: bold;
+  font-family: $roboto;
+  text-align: center;
+  cursor: pointer;
+  transition: 0.4s;
+  display: none;
+  z-index: 9;
+  &:hover {
+    color: $black;
+    background: $white;
+    img {
+      filter: invert(0);
+    }
+  }
+  img {
+    width: 60px;
+    margin: auto;
+    filter: invert(1);
+    transition: 0.4s;
+  }
+  p {
+    margin: -10px 0 10px;
+  }
+}
+.visible{
+    display: block !important;
+}
+</style>
